49a9d7bbd016220300a989dd32860ceeeffaccfe
Hetpatel4699 <hetpatel4699@gmail.com>
2024-06-19 18:33:47 +0530

    5th commit

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -10,6 +10,12 @@ const About =()=>{
         <div>
             <h1>About Page</h1>
             <button onClick={()=>router.push("./")}>Go To Home Page </button>
+            <br/>
+            <br/>
+            <Link href="/about/aboutcollege">Go To About College Page</Link>
+            <br/>
+            <br/>
+            <Link href="/about/aboutstudent">Go To About Student Page</Link>
             
 
         </div>
--- src/app/login/page.js
+++ src/app/login/page.js
@@ -1,10 +1,22 @@
+"use client"
 import Link from "next/link";
+import {useRouter} from "next/navigation"
 
 const Login=()=>{
+    const router =useRouter();
+    const navigate= (page)=> {
+        router.push("login" + page);
+    }
     return(
         <div>
             <h1>Login Page</h1>
             <Link href="/">Go To Home Page</Link>
+            <br/>
+            <br/>
+            <button onClick={()=>navigate("/loginstudent")}>Go To Student Login Page</button>
+            <br/>
+            <br/>
+            <button onClick={()=>navigate("/loginteacher")}>Go To Teacher Login Page</button>
         </div>
     )
 }
