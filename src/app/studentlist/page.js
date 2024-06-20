170f9f1a737403b039d9c96c597eca7b63fc9fff
Hetpatel4699 <hetpatel4699@gmail.com>
2024-06-20 17:38:56 +0530

    8th commit

---
+++ src/app/studentlist/page.js
@@ -0,0 +1,23 @@
+import Link from "next/link";
+
+export default function StudentList(){
+    return(
+        <div>
+            <h1>Student List Page</h1>
+            <ul>
+                <li>
+                    <Link href="./studentlist/het">Het</Link>
+                </li>
+                <li>
+                    <Link href="./studentlist/harsh">Harsh</Link>
+                </li>
+                <li>
+                    <Link href="./studentlist/siddharth">Siddharth</Link>
+                </li>
+                <li>
+                    <Link href="./studentlist/chirag">Chirag</Link>
+                </li>
+            </ul>
+        </div>
+    )
+}
