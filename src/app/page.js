8eab86df423cf283134931dbfed43402228ed499
Hetpatel4699 <hetpatel4699@gmail.com>
2024-06-28 12:48:20 +0530

    13th commit

--- src/app/page.js
+++ src/app/page.js
@@ -8,7 +8,7 @@ export default function Home() {
  
   return (
    <main>
-    <h1>Fetch Data with API in Client Component</h1>
+    <h1>Fetch Data with API in Server Component</h1>
     <Link href="/productlist">Go To Product List</Link>
    </main>
   );
