742d84ecf456c3059e77a4a2e6518a4447bb1d19
Hetpatel4699 <hetpatel4699@gmail.com>
2024-07-01 18:20:56 +0530

    16th commit

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,14 @@
-// pages/_app.js or pages/_app.tsx      
+import custom from './custom.module.css'
+import other from './other.module.css'
+import outside from '@/style/outside.module.css'
 
 export default function Home() {
     return (
       <main>
-        <h1>Style and CSS with Next js</h1>
-        <h2>WELCOME</h2>
+        <h1 className={custom.main}>CSS Modules with Next js</h1>
+        <h2 className={other.main}>CSS Modules with Next js</h2>
+        <h3 className={outside.main}>CSS Modules with Next js</h3>
+        
       </main>
     )
   }
