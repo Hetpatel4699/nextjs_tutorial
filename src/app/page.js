9eef2792da89d6e50c5a085e578321bcd314de46
Hetpatel4699 <hetpatel4699@gmail.com>
2024-07-02 11:42:38 +0530

    17th commit

--- src/app/page.js
+++ src/app/page.js
@@ -1,14 +1,21 @@
-import custom from './custom.module.css'
-import other from './other.module.css'
-import outside from '@/style/outside.module.css'
+"use client"
+import { useState } from 'react'
+import style from './style.module.css'
+export default function Home(){
 
-export default function Home() {
-    return (
-      <main>
-        <h1 className={custom.main}>CSS Modules with Next js</h1>
-        <h2 className={other.main}>CSS Modules with Next js</h2>
-        <h3 className={outside.main}>CSS Modules with Next js</h3>
-        
-      </main>
-    )
-  }
+  const [color,setColor]= useState("red");
+  const {red}= style
+  return(
+    <main>
+      <h1 className={color=="red"?style.red:style.green}>Condition with Style</h1>
+      <h2 style={{backgroundColor:color=="red"?"red":"green"}}>Heading 2</h2>
+      <h3 id={style.orange}>Heading 3</h3>
+
+      <h4 className={red}>Dummy Text</h4>
+      <h4 className={red}>Dummy Text</h4>
+      <h4 className={red}>Dummy Text</h4>
+      <h4 className={red}>Dummy Text</h4>
+      <button onClick={()=>setColor("green")}>Update Color</button>
+    </main>
+  )
+}
